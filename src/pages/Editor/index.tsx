33e531d0496e9262d7ed86fb29380fe9f2685261
cartoonweber <minkbuckstar@gmail.com>
2023-05-24 05:46:46 -0700

    fix impact font with white color

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -304,7 +304,6 @@ export default function EditorPage() {
             fontFamilyOptions: [
               // Add our custom fonts
               ['Impact', 'Impact'],
-              ['Impact-meme', 'Meme'],
               ['Arial', 'Arial'],
               ['Helvetica', 'Helvetica'],
               ['Montserrat', 'Montserrat'],
